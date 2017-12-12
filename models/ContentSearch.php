dd41983ce026153d8ea644cc4f2c49e77ce46a3c
Uğur YILDIZ <yildiz.ugur@gmail.com>
2017-12-12 13:42:05 +0300

    Module created

--- models/ContentSearch.php
+++ models/ContentSearch.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace app\models;
+namespace kouosl\content\models;
 
 use Yii;
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
-use app\models\Content;
+use kouosl\content\models\Content;
 
 /**
  * ContentSearch represents the model behind the search form about `app\models\Content`.
